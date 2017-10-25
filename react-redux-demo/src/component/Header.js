60667d3415759874af33bf970acc607665ab1b70
李杨 <liyang@shencom.cn>
2017-10-25 19:11:23 +0800

    Add react-redux-demo

---
+++ react-redux-demo/src/component/Header.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from './react-redux';
+
+class Header extends Component {
+  static contextTypes = {
+    store: PropTypes.object
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 style={{
+          color: this.props.themeColor
+        }}>react-demo</h1>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+
+Header = connect(mapStateToProps)(Header)
+
+export default Header;
